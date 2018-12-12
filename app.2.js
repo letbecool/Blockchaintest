86648c602ff4d807720dba307c9528fbb1ce5ba9
bikeshrestha <srpbsm@gmail.com>
2018-12-12 21:04:13 +0545

    pending transaction

--- app.2.js
+++ app.2.js
@@ -4,48 +4,17 @@ fs = require('fs')
 
 // set the provider you want from Web3.providers
 var net = require('net')
-var web3 = new Web3('/home/bikeshrestha/awesomeProject/Blockchaintest/nodes/node2/geth.ipc', net);
+var web3 = new Web3('../.cobuna/geth.ipc', net);
 //var web3 = new Web3("ws://localhost:8502")
 
 console.log("connected to local network")
 
 const args = process.argv.slice(2)
-// for counting empty block to exit
-count=0
-counttx=0
-NOOFTX=20000
-
-
-/*
-fs.writeFile(`${__dirname}/outputs/web3input.csv`, '' , err => {
-    if (!err) {
-        console.log('file empty')
-    } else {
-        console.log(err)
-    }
-})
-*/
-/*
-fs.writeFile(`${__dirname}/outputs/web3output.csv`, '' , err => {
-
-    if (!err) {
-
-        console.log('file empty')
-
-    } else {
-
-        console.log(err)
-
-    }
-
-
-})
-
-*/
 
 let i = 0;
 let j = 0;
 web3.eth.defaultAccount = web3.eth.accounts[0]
+
 //web3.eth.personal.unlockAccount(web3.eth.defaultAccount,"testing",0)
 
 const abi = [
@@ -110,16 +79,11 @@ const abi = [
 ]
 var contractAddress, tps;
 if(args[1] != undefined){
-	contractAddress = args[1].toString();
+	contractAddress = args[0].toString();
 }else{
  	contractAddress = '0x0864c263423928ffe47560e1daa3d5fa9490447d';
 }
 
-if(args[0] != undefined){
-	tps = parseInt(args[0]);
-}else{
-	tps = 10;
-}
 
 const contractInstance = new web3.eth.Contract(abi,contractAddress);
 /*
@@ -139,10 +103,6 @@ console.log(error)
 });
 
 */
-
-
-
-
 
 
 contractInstance.events.LogOfCauidGen()
@@ -172,41 +132,9 @@ web3.eth.extend({
 	}]
   });
  
-//var b = 0;
-
-// subscribe to event when new block is created
-
-web3.eth.subscribe('newBlockHeaders', (error, block) => {
-        if(!error) {
-    web3.eth.getBlock(block.number).then(function(value){
-	web3.eth.txpool.status().then(function(result){
- 	console.log(result.pending);
-	console.log(value.size)
-	utils.saveInputToCsv(`${value.number}, ${value.timestamp} ,${value.gasUsed},${value.size},${value.transactions.length}, ${parseInt(result.pending)} , ${value.difficulty}, ${value.totalDifficulty}`, d.getDate()+'_'+tps+'.csv');
-	/*
-	if(b==20){
-		process.exit(1)
-	}
-	b++
-	*/
-	
-})
-}).catch(console.error)
-
-        } else {
-            console.log('Error:', error);          
-        }
-});
-
-
-
 // pending transaction every 1 second
 
-setInterval(pendingTxEverySecond,1000);
-
-
-// pending transaction every second and logs to output
-var pendingTxEverySecond = function() {
+setInterval(function(){
 	web3.eth.getBlockNumber().then(function(block){
 		web3.eth.getBlock(block).then(function(value){
 			web3.eth.txpool.status().then(function(result){
@@ -214,7 +142,8 @@ var pendingTxEverySecond = function() {
 		})
 	})
 	})
-}
+	
+},1000)
 
 /*
 // for certain time 
@@ -277,53 +206,3 @@ var x=setInterval(doStuff1,1000/tps,time);
 */
 
 
-//sending certainer number of transction suppose say 120000
-var doStuff1 = function () {
-	if(counttx<NOOFTX) {
-		let iid = utils.genRandomStr(8)
-		let plid = utils.genRandomStr(8)
-		let uid = i
-		//contractInstance.getID(i, iid, plid,{gas:32000})
-		contractInstance.methods
-		.getID(i, iid, plid)
-		.send({
-			from: '0x142612093efca0f055d10476493ca9a63b6e436c',
-			gas: 50000,
-			gasPrice: 100000000000
-		},function (error, value) {
-			if(!error){
-				utils.saveInputToCsv(`${i} ,${plid} ,${iid} ,${utils.getCurentDateTime()}`, d.getDate()+'_'+'web3input'+'_'+tps+'.csv');
-			} 
-		}).catch(console.log)
-		console.log(i)
-		counttx++
-		i++
-	}else{
-		clearInterval(x)
-		console.log("Completed.......Completed........Completed")
-		web3.eth.subscribe('newBlockHeaders', (error, block) => {
-			if(!error) {
-		web3.eth.getBlock(block.number).then(function(value){
-		web3.eth.txpool.status().then(function(result){
-		pending=parseInt(result.pending);
-		
-		if(pending==0 || count == 10){
-			process.exit(1)
-		}	
-	
-		tx=value.transactions.length
-		if(tx==0){
-			count++
-		}
-		
-		})
-		}).catch(console.error)
-		
-			} else {
-				console.log('Error:', error);          
-			}
-		});
-		
-	}};
-
-	var x=setInterval(doStuff1,1000/tps);
